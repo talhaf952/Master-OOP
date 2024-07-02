66fa9d3925da4a12e762c2a112d0e9b105b28b12
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2024-07-02 23:09:50 +0500

    Function details

---
+++ Function Details.cpp
@@ -0,0 +1,40 @@
+
+#include<iostream>
+using namespace std;
+
+int sum(int a,int b)
+{
+    int c=a+b;
+    return c;
+}
+
+/*
+1. Return Type
+2. Function Name
+3. Parameter List
+4. Function Body
+5. Return Value
+6. Function Call
+
+Function Definition(Return Type, Function Name, Parameter List, Function Body, Return Value)
+
+Function Declaration(Return Type, Function Name, Parameter List)
+
+Function Signature(Function Name, Parameter List)
+
+*/
+
+int main()
+{
+    cout<<"Sum Program:\n";
+    cout<<"Sum = "<<sum(5,10);
+    return 0;
+}
+
+/*
+//  4. Pillars of OOP
+1. Abstraction
+2. Inheritance
+3. Polymorphism
+4. Encapsulation
+*/
